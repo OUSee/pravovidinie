291c905d2dcd29ee23b51b4fb1d763ed8ba6d56c
OUSee <666rld999@mail.ru>
2025-07-02 11:07:41 +0300

    tryout media

--- src/components/VideoChat/VideoWindow.vue
+++ src/components/VideoChat/VideoWindow.vue
@@ -1,12 +1,57 @@
 <script setup lang="ts">
 import './VideoChat.scss'
 import VideoChatBar from './VideChatBar.vue'
-import { inject } from 'vue'
+import { inject, watchEffect, ref } from 'vue'
 
-const refVideoStream = inject('refVideo');
-const refUserVideoStream = inject('refUserVideo');
+const refVideoStream = inject<any>('refVideo');
+const refUserVideoStream = inject<any>('refUserVideo');
+
+const yourVideo = ref<HTMLVideoElement | null>(null)
+const partnerVideo = ref<HTMLVideoElement | null>(null)
 
 
+const handleVideoStream = () => {
+    if (yourVideo.value !== null && refUserVideoStream.value) {
+        console.log('Setting srcObject for yourVideo');
+        refUserVideoStream.getTracks().forEach((track: any) => {
+            console.log(`Track ${track.kind} enabled:`, track.enabled);
+            track.enabled = true;
+        });
+        yourVideo.value.muted = true
+        yourVideo.value.srcObject = refUserVideoStream.value;
+    }
+};
+
+watchEffect(() => {
+    console.log(refVideoStream.value);
+    console.log(refUserVideoStream.value);
+})
+
+watchEffect(() => {
+    if (refVideoStream.value !== null && refVideoStream.value !== undefined && partnerVideo.value !== null) {
+        partnerVideo.value.srcObject = refVideoStream.value;
+    }
+});
+
+watchEffect(() => {
+    handleVideoStream()
+
+    if (yourVideo.value !== null && !refUserVideoStream.value) {
+        const observer = new MutationObserver(() => {
+            if (refUserVideoStream.value) {
+                handleVideoStream();
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+        return () => observer.disconnect();
+    }
+})
 
 </script>
 
